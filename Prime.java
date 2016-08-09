f5279cd7e8dc5c471ef73a5e6ffa7b3c6c1c3519
SoundaryaShanmugam04 <suk.ietcse13@gmail.com>
2016-08-09 21:17:57 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Prime {
+	public static void prime(int n)
+	{
+	ArrayList<Integer> als=new ArrayList<Integer>();
+	int num=0;
+	int n1;
+	}
+	public static void main(String args[])
+	{
+	for(int i=0;i<n;i++)
+		System.out.println("enter a num");
+	
+	for(int j=0;j<n;j++)
+	{
+		if(i%j==0)		
+	}
+}
+}
